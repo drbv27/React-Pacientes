a5a3bc61c261a91a4a44c3deef6ba4c70b380179
Diego <drbv27@gmail.com>
2022-01-15 13:17:47 -0500

    First component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo</h1>
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
   );
 }
 
