172fcd5783e89616193ee1052e6cfc055632a076
Diego <drbv27@gmail.com>
2022-01-15 14:08:23 -0500

    some styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 function App() {
   return (
-    <>
-      <div>
-        <Header />
-        <Formulario />
-        <ListadoPacientes />
-      </div>
-    </>
+    <div className="container mx-auto mt-20">
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+    </div>
   );
 }
 
