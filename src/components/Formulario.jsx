84ade395ae17ca9ed78610badcee85fd0f5bce4b
Diego <drbv27@gmail.com>
2022-01-18 21:26:49 -0500

    Error component

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Error from "./Error";
 
 const Formulario = ({ pacientes, setPacientes }) => {
   const [nombre, setNombre] = useState("");
@@ -46,11 +47,7 @@ const Formulario = ({ pacientes, setPacientes }) => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error ? (
-          <div className="bg-red-700 text-white text-center p-3 uppercase rounded-md font-bold mb-3">
-            <p>"Todos los campos son obligatorios"</p>
-          </div>
-        ) : null}
+        {error ? <Error mensaje='"Todos los campos son obligatorios"' /> : null}
         {/* {error && "Si hay un error"} */}
         <div className="mb-5">
           <label
