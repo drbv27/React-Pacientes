d0350b8a8b7515b74bb4f15551db177d721c451b
Diego <drbv27@gmail.com>
2022-01-20 10:23:06 -0500

    Conditional showing text

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,15 +3,31 @@ import Paciente from "./Paciente";
 const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl text-center mt-5 mb-10">
-        Adiministra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl text-center mt-5 mb-10">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
 
-      {pacientes.map((paciente) => (
-        <Paciente key={paciente.id} paciente={paciente} />
-      ))}
+          {pacientes.map((paciente) => (
+            <Paciente key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            No hay pacientes registrados
+          </h2>
+          <p className="text-xl text-center mt-5 mb-10">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              y apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
