8d2ee347a281e3f8224db45c20e85a3e3b8a5223
Diego <drbv27@gmail.com>
2022-01-20 10:31:53 -0500

    Adding edit and delete buttons

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -19,6 +19,20 @@ const Paciente = ({ paciente }) => {
         Sintomas:{""}{" "}
         <span className="font-normal normal-case">{sintomas}</span>
       </p>
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white rounded-md font-bold uppercase"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-800 text-white rounded-md font-bold uppercase"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 };
