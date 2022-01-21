a3966efb1d0ffd180ebef8294923e1e9795e9113
Diego <drbv27@gmail.com>
2022-01-21 14:32:22 -0500

    puting on the state the patience to edit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,11 @@ function App() {
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Formulario pacientes={pacientes} setPacientes={setPacientes} />
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+        />
         <ListadoPacientes pacientes={pacientes} setPaciente={setPaciente} />
       </div>
     </div>
