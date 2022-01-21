47a342b3ca6a9d6d9379cb06c922709dd64e1227
Diego <drbv27@gmail.com>
2022-01-21 13:53:29 -0500

    Registering an event with edit button

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,4 +1,4 @@
-const Paciente = ({ paciente }) => {
+const Paciente = ({ paciente, setPaciente }) => {
   const { nombre, propietario, email, fecha, sintomas } = paciente;
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
@@ -23,6 +23,7 @@ const Paciente = ({ paciente }) => {
         <button
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-800 text-white rounded-md font-bold uppercase"
+          onClick={() => setPaciente(paciente)}
         >
           Editar
         </button>
